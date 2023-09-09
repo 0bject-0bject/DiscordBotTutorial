9380218e357948a76d3041d33dfb237c060c91d4
Jimmy1116 <117414402+Jimmy1116@users.noreply.github.com>
2023-09-09 17:10:48 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const {
+    Client,
+    Collection,
+    GatewayIntentBits,
+} = require('discord.js');
+
+const client = new Client({
+    intents: [ 
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+    ],
+});
+
+require('dotenv').config();
+
+const fs = require('fs');
+
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    if (!command.name || !command.execute) continue;
+    client.commands.set(command.name, command);
+}
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+
+    if (!event.name || !event.execute) continue;
+
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
+}
+
+client.login(process.env.TOKEN);
